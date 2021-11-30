c072410714e015074c1a29079fce21dd7b3b9229
WesternGamer <kevnguyen474@gmail.com>
2021-11-30 18:15:02 -0500

    Fixed issue where an exception would occur if some folders did exist.

--- Main.cs
+++ Main.cs
@@ -1,6 +1,5 @@
-﻿using HarmonyLib;
+﻿using System;
 using System.IO;
-using System.Reflection;
 using VRage.FileSystem;
 using VRage.Plugins;
 
@@ -15,237 +14,306 @@ namespace FileCleaner
 
         public void Init(object gameInstance)
         {
-            #region Cache
-            DirectoryInfo directoryInfoCache = new DirectoryInfo(MyFileSystem.CachePath);
+            #region Cache        
+            try
+            {
+                DirectoryInfo directoryInfoCache = new DirectoryInfo(MyFileSystem.CachePath);
 
-            foreach (FileInfo file in directoryInfoCache.EnumerateFiles())
-            {
-                try
+                foreach (FileInfo file in directoryInfoCache.EnumerateFiles())
                 {
-                    file.Delete();
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
                 }
-                catch 
+                foreach (DirectoryInfo dir in directoryInfoCache.EnumerateDirectories())
                 {
-                    continue;
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
                 }
-                
             }
-            foreach (DirectoryInfo dir in directoryInfoCache.EnumerateDirectories())
+            catch (Exception)
             {
-                try
-                {
-                    dir.Delete(true);
-                }   
-                catch
-                {
-                    continue;
-                }
+
             }
             #endregion
             #region IngameScriptsTemp
-            DirectoryInfo directoryInfoIngameScriptsTemp = new DirectoryInfo(Path.Combine(MyFileSystem.CachePath, "..\\IngameScripts\\temp"));
-
-            foreach (FileInfo file in directoryInfoIngameScriptsTemp.EnumerateFiles())
+            try
             {
-                try
-                {
-                    file.Delete();
-                }
-                catch
-                {
-                    continue;
-                }
+                DirectoryInfo directoryInfoIngameScriptsTemp = new DirectoryInfo(Path.Combine(MyFileSystem.CachePath, "..\\IngameScripts\\temp"));
 
+                foreach (FileInfo file in directoryInfoIngameScriptsTemp.EnumerateFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                }
+                foreach (DirectoryInfo dir in directoryInfoIngameScriptsTemp.EnumerateDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
             }
-            foreach (DirectoryInfo dir in directoryInfoIngameScriptsTemp.EnumerateDirectories())
+            catch (Exception)
             {
-                try
-                {
-                    dir.Delete(true);
-                }
-                catch
-                {
-                    continue;
-                }
+
             }
             #endregion
             #region ProfileOptimization
-            DirectoryInfo directoryInfoProfileOptimization = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "ProfileOptimization"));
-
-            foreach (FileInfo file in directoryInfoProfileOptimization.EnumerateFiles())
+            try
             {
-                try
-                {
-                    file.Delete();
-                }
-                catch
-                {
-                    continue;
-                }
+                DirectoryInfo directoryInfoProfileOptimization = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "ProfileOptimization"));
 
+                foreach (FileInfo file in directoryInfoProfileOptimization.EnumerateFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                }
+                foreach (DirectoryInfo dir in directoryInfoProfileOptimization.EnumerateDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
             }
-            foreach (DirectoryInfo dir in directoryInfoProfileOptimization.EnumerateDirectories())
+            catch (Exception)
             {
-                try
-                {
-                    dir.Delete(true);
-                }
-                catch
-                {
-                    continue;
-                }
+
             }
             #endregion
             #region Promo
-            DirectoryInfo directoryInfoPromo = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "Promo"));
-
-            foreach (FileInfo file in directoryInfoPromo.EnumerateFiles())
+            try
             {
-                try
-                {
-                    file.Delete();
-                }
-                catch
-                {
-                    continue;
-                }
+                DirectoryInfo directoryInfoPromo = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "Promo"));
 
+                foreach (FileInfo file in directoryInfoPromo.EnumerateFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                }
+                foreach (DirectoryInfo dir in directoryInfoPromo.EnumerateDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
             }
-            foreach (DirectoryInfo dir in directoryInfoPromo.EnumerateDirectories())
+            catch (Exception)
             {
-                try
-                {
-                    dir.Delete(true);
-                }
-                catch
-                {
-                    continue;
-                }
+
             }
             #endregion
             #region ShaderCache2
-            DirectoryInfo directoryInfoShaderCache2 = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "ShaderCache2"));
-
-            foreach (FileInfo file in directoryInfoShaderCache2.EnumerateFiles())
+            try
             {
-                try
-                {
-                    file.Delete();
-                }
-                catch
-                {
-                    continue;
-                }
+                DirectoryInfo directoryInfoShaderCache2 = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "ShaderCache2"));
 
+                foreach (FileInfo file in directoryInfoShaderCache2.EnumerateFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                }
+                foreach (DirectoryInfo dir in directoryInfoShaderCache2.EnumerateDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
             }
-            foreach (DirectoryInfo dir in directoryInfoShaderCache2.EnumerateDirectories())
+            catch (Exception)
             {
-                try
-                {
-                    dir.Delete(true);
-                }
-                catch
-                {
-                    continue;
-                }
+
             }
             #endregion
             #region temp
-            DirectoryInfo directoryInfotemp = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "temp"));
-
-            foreach (FileInfo file in directoryInfotemp.EnumerateFiles())
+            try
             {
-                try
-                {
-                    file.Delete();
-                }
-                catch
-                {
-                    continue;
-                }
+                DirectoryInfo directoryInfotemp = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "temp"));
 
+                foreach (FileInfo file in directoryInfotemp.EnumerateFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                }
+                foreach (DirectoryInfo dir in directoryInfotemp.EnumerateDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
             }
-            foreach (DirectoryInfo dir in directoryInfotemp.EnumerateDirectories())
+            catch (Exception)
             {
-                try
-                {
-                    dir.Delete(true);
-                }
-                catch
-                {
-                    continue;
-                }
+
             }
             #endregion
             #region WorkshopBrowser
-            DirectoryInfo directoryInfoWorkshopBrowser = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "WorkshopBrowser"));
-
-            foreach (FileInfo file in directoryInfoWorkshopBrowser.EnumerateFiles())
+            try
             {
-                try
-                {
-                    file.Delete();
-                }
-                catch
-                {
-                    continue;
-                }
+                DirectoryInfo directoryInfoWorkshopBrowser = new DirectoryInfo(Path.Combine(MyFileSystem.UserDataPath, "WorkshopBrowser"));
 
+                foreach (FileInfo file in directoryInfoWorkshopBrowser.EnumerateFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                }
+                foreach (DirectoryInfo dir in directoryInfoWorkshopBrowser.EnumerateDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
             }
-            foreach (DirectoryInfo dir in directoryInfoWorkshopBrowser.EnumerateDirectories())
+            catch (Exception)
             {
-                try
-                {
-                    dir.Delete(true);
-                }
-                catch
-                {
-                    continue;
-                }
+
             }
             #endregion
             #region GPUCache
-            DirectoryInfo directoryInfoGPUCache = new DirectoryInfo(Path.Combine(MyFileSystem.ExePath, "GPUCache"));
-
-            foreach (FileInfo file in directoryInfoGPUCache.EnumerateFiles())
+            try
             {
-                try
-                {
-                    file.Delete();
-                }
-                catch
-                {
-                    continue;
-                }
+                DirectoryInfo directoryInfoGPUCache = new DirectoryInfo(Path.Combine(MyFileSystem.ExePath, "GPUCache"));
 
+                foreach (FileInfo file in directoryInfoGPUCache.EnumerateFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                }
+                foreach (DirectoryInfo dir in directoryInfoGPUCache.EnumerateDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
             }
-            foreach (DirectoryInfo dir in directoryInfoGPUCache.EnumerateDirectories())
+            catch (Exception)
             {
-                try
-                {
-                    dir.Delete(true);
-                }
-                catch
-                {
-                    continue;
-                }
+
             }
             #endregion
 
-            File.Delete(Path.Combine(MyFileSystem.UserDataPath, "mipMapTextureCache2.B5"));
-
-
-            foreach (string sFile in Directory.GetFiles(MyFileSystem.UserDataPath, "*.log"))
+            try
             {
-                try
+                File.Delete(Path.Combine(MyFileSystem.UserDataPath, "mipMapTextureCache2.B5"));
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                foreach (string sFile in Directory.GetFiles(MyFileSystem.UserDataPath, "*.log"))
                 {
-                    File.Delete(sFile);
-                }
-                catch
-                {
-                    continue;
+                    try
+                    {
+                        File.Delete(sFile);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
                 }
             }
+            catch (Exception)
+            {
+
+            }
+            
         }
 
         public void Update()
